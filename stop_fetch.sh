4aa60d33766e9caf4a222363a9275af466353b59
Theo Turner <theo_turner@outlook.com>
2019-08-14 16:38:49 +0100

    Node stopper now kills MT core and Pluto

--- stop_fetch.sh
+++ stop_fetch.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-onred='\033[41m'
 ongreen='\033[42m'
 onyellow='\033[43m'
-endcolor="\033[0m"
+endcolor='\033[0m'
 
-# Handle errors
-set -e
-error_report() {
-    echo -e "${onred}Error: failed on line $1.$endcolor"
-}
-trap 'error_report $LINENO' ERR
+echo -e "${onyellow}Stopping nodes...$endcolor"
 
-echo -e "${onyellow}Stopping Fetch node...$endcolor"
-docker stop $(docker ps | grep oef-core-image | awk '{ print $1 }')
-echo -e "${onred}Fetch node stopped.$endcolor"
+docker stop $(docker ps -a | grep oef-search | awk '{print $1}') &> /dev/null
+kill -INT $(ps | grep oef-mt-core | awk '{print $1}') &> /dev/null
+
+echo -e "${ongreen}Nodes stopped.$endcolor"
