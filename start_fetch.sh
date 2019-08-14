942a5819f1b33e3f91fc10fc47b0a74f23eee890
Theo Turner <theo_turner@outlook.com>
2019-08-14 16:37:44 +0100

    Update node starter for MT core and pluto

--- start_fetch.sh
+++ start_fetch.sh
@@ -5,6 +5,8 @@ ongreen='\033[42m'
 onyellow='\033[43m'
 endcolor="\033[0m"
 
+docker info &> /dev/null || (echo -e "${onred}Error: Docker is not running.$endcolor" && exit 1)
+
 # Handle errors
 set -e
 error_report() {
@@ -13,5 +15,8 @@ error_report() {
 trap 'error_report $LINENO' ERR
 
 echo -e "${onyellow}Starting Fetch node...$endcolor"
-./oefcore/oef-core-image/scripts/docker-run.sh -p 3333:3333 -d --
+cd oef-search-pluto
+python3 scripts/launch.py -c ./scripts/launch_config.json --background &> /dev/null
+cd ../oef-mt-core
+bazel run mt-core/main/src/cpp:app -- --config_file `pwd`/mt-core/main/src/cpp/config.json &> /dev/null &
 echo -e "${ongreen}Fetch node is running.$endcolor"
